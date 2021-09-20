ed554069d911fc2e46212cfc17db1afb9d811744
Hanno Embregts <hanno.embregts@infosupport.com>
2021-09-20 11:58:38 +0200

    Add requirement numbers in solution

--- assignments/slow-numbers/src/main/java/SlowNumbers.java
+++ assignments/slow-numbers/src/main/java/SlowNumbers.java
@@ -1,22 +1,23 @@
 public class SlowNumbers {
     public static void main(String[] args) throws InterruptedException {
-        Thread slowNumbers = new Thread(() -> {
+        Thread slowNumbers = new Thread(() -> { // 3.
             for (int number = 0; number < 10; number++) {
-                System.out.println(number);
+
+                System.out.println(number); // 1.
 
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(1000); // 2.
                 } catch (InterruptedException e) {
                     System.out.println("Thread interrupted; discarding work...");
-                    break;
+                    break; // 7.
                 }
             }
         });
 
-        slowNumbers.start();
-        Thread.sleep(3000);
+        slowNumbers.start(); // 4.
+        Thread.sleep(3000); // 4.
 
-        slowNumbers.interrupt();
-        Thread.sleep(1000);
+        slowNumbers.interrupt(); // 5.
+        Thread.sleep(1000); // 6.
     }
 }
